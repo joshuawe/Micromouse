73337568ae86a5e4234e5aa54d69a40627c157f8
Michelle <mibettendorf@yahoo.com>
2022-01-25 13:31:40 +0100

    Bestellliste aktualisiert + MotorControl.c

---
+++ code/MyTestProject.X/ICC.c
@@ -0,0 +1,38 @@
+/*
+ * File:   ICC.c
+ * Author: mibet
+ *
+ * Created on 25. Januar 2022, 10:41
+ */
+
+
+#include "xc.h"
+#include "MotorControl.h"
+
+int main(void) {
+    return 0;
+}
+
+void drive(char next_step)
+{
+    if(next_step == "l")
+    {
+        left_90degree();
+    }
+    else if(next_step == "r")
+    {
+        right_90degree();
+    }
+    else if(next_step == "f")
+    {
+        drive_forward();
+    }
+    else if (next_step == "b")
+    {
+        drive_backward();
+    }
+    else if (next_step == "t")
+    {
+        turning();
+    }
+}
