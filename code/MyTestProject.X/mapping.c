266012c694bf5bc7c35c86bf18ec72b8f849ef24
Michelle <mibettendorf@yahoo.com>
2022-03-20 21:29:06 +0100

    explore mode added

--- code/MyTestProject.X/mapping.c
+++ code/MyTestProject.X/mapping.c
@@ -11,6 +11,7 @@
  */
 
 #include "xc.h"
+#include <stdbool.h>
 #define squares 10
 
 typedef struct {
@@ -28,7 +29,7 @@ double right_measurement; // in cm
 double left_measurement; // in cm
 int i;
 int orientation; //front = 0, turned right = 1, back = 2 turned left = 3
-
+bool visited[squares][squares];
 cell map[squares][squares];
 
 void set_maze_size(int size)
@@ -61,7 +62,7 @@ void set_measurements(double new_front_measurement, double new_right_measurement
 }
 
 /*
- * free = 0, obstacle  = 1, unsure = 2
+ * free = 0, obstacle  = 1, unsure = -1
  * binary: bottom, left, up, right
  * connections numbers start from left bottom with 0,1,2,3 then going one square up: 
  */
@@ -76,9 +77,11 @@ void initalize_map()
             map[o][j].left = -1;
             map[o][j].right = -1;
             map[o][j].front = -1;
+            visited[o][j] = false;
             
         }
     }        
+    visited[0][0] = true;
     
    // squares = (int)maze_size/square_size;
     for (i = 0; i < squares; i++)
@@ -269,3 +272,85 @@ void update_map(double new_front_measurement, double new_right_measurement, doub
     }
 }
 
+void explore(int positionx, int positiony)
+{
+    if(explorefront(int positionx, int positiony)== -1)
+    {
+        if(exploreleft(int positionx, int positiony) == -1)
+        {
+            if(exploreright(int positionx, int positiony) == -1)
+            {
+                if(allexplored() == -1)
+                {
+                    move(int positionx, int positiony);
+                }
+            }
+        }
+    }
+}
+
+int exploreleft(int positionx, int positiony)
+{
+    if(positionx == 0 || map[positionx][positiony].left == 1) // if the robot is on the very left of the maze or there is a wall on the left
+    {
+        return -1;
+    }    
+    else if(visited[positionx-1][positiony] == true) //left square was already visited
+    {
+        return -1;
+    }
+    
+    //turn to the left and drive one square forward
+    return 1;
+}
+
+int explorefront(int positionx, int positiony)
+{
+    if(positionx == squares -1 || map[positionx][positiony].front == 1) // if the robot is on the very top of the maze or there is a wall on the left
+    {
+        return -1;
+    }    
+    else if(visited[positionx][positiony+1] == true) //left square was already visited
+    {
+        return -1;
+    }
+    
+    // drive one square forward
+    return 1;
+}
+
+int exploreright(int positionx, int positiony)
+{
+    if(positionx == squares -1 || map[positionx][positiony].right == 1) // if the robot is on the very left of the maze or there is a wall on the left
+    {
+        return -1;
+    }    
+    else if(visited[positionx+1][positiony] == true) //left square was already visited
+    {
+        return -1;
+    }
+    
+    //turn to the right and drive one step forward
+    return 1;
+}
+
+int allexplored()
+{
+    int k,l;
+    for(k = 0; k < squares; k++)
+    {
+        for(l = 0; l < squares; l++)
+        {
+            if(visited[k][l] == false)
+            {
+                return -1;
+            }
+        }
+    }
+    return 1;
+}
+
+void move(int positionx, int positiony)
+{
+    //try to move somewhere new
+}
