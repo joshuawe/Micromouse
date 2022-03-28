49fb9af6fdc969c825fb3be30c06704a4d09d43e
Joshua Wendland <joshua.wendland@tum.de>
2022-03-28 16:09:57 +0200

    proper parsing and passing of variables

--- BT_communication/connect.py
+++ BT_communication/connect.py
@@ -40,33 +40,48 @@ class SerialHandler():
         print(" -> connected")
 
 
-    def parseLines(self, lines:'list[str]'):
-        delimiter = self.settings["variable_parse"]["delimiter"]
-        splittings = self.settings["variable_parse"]["format"].count(delimiter) + 1 # The number of individual parts of the message
-        signature = self.settings["variable_parse"]["signature"]
+    def parseLines(self, lines:'list[str]', verbose=False):
+        """Takes a list of strings and checks if it understands each line.
+            A message could look like this (depending on format defined in config file):
+                      signature:varName:varValue
+                        |          |        |
+                 message[0]  message[1]  message[2]
+
+        Args:
+            lines (list[str]): List of lines to check.
+            verbose (boolean): Wether conosle prints for debugging should be executed.
+        """
+        delimiter = self.settings["variableParse"]["delimiter"]
+        splittings = self.settings["variableParse"]["format"].count(delimiter) + 1 # The number of individual parts of the message
+        signature = self.settings["variableParse"]["signature"]
         varKeys = self.settings["variables"].keys()
+        newVars = dict()
         # Go through all the lines
         for i,line in enumerate(lines):
-            print(f"Line {i}\t", end="")
+            if verbose: print(f"Line {i}\t", end="")
             message = line.strip()
             message = message.split(delimiter)
-            if len(message) > splittings:
+            if (len(message) != splittings):
                 # Invalid, could be that a delimiter was used inside the variable content
-                print(f"Invalid input, too many delimiters: '{line}'")
-            elif (message[0] != signature) or (message[-1] != signature):
+                if verbose: print(f"Invalid input, too many delimiters: '{line}'")
+            elif (message[0] != signature):
                 # Invalid signature
-                print(f"Invalid, Wrong signature: '{line}'")
+                if verbose: print(f"Invalid, Wrong signature, Expected {signature}, got {message[0]}: '{line}'")
             elif message[1] not in varKeys:
                 # Invalid variable name
-                print(f"Invalid variable name: '{line}'")
+                if verbose: print(f"Invalid variable name: '{line}'")
             else:
-                print(f"Correct parsing: '{line}'")
+                newVars[message[1]] = message[2]
+                if verbose: print(f"Correct parsing: {message[1]} = {message[2]}")
+
+        print("All variables:")
+        print(newVars)
 
 
 
 
 
-    def collectLines(self, lines):
+    def collectLines(self, lines, consolePrint=True):
         """Print X many lines from the serial. Connection has to be established already.
 
         Args:
@@ -79,7 +94,7 @@ class SerialHandler():
         for i in range(lines):
             serial_bytes = self.serial.readline()
             message = serial_bytes.decode()
-            print(message, end="")
+            if consolePrint: print(message, end="")
             messages.append(message)
 
         return messages
